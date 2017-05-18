09743c7478828934059006cfc2ba77297dbd3d96
godjan <g0dj4n@gmail.com>
2017-05-18 17:14:12 +0500

    Resources merged

--- YOBAGame/MapObjects/UsualWeapon.cs
+++ YOBAGame/MapObjects/UsualWeapon.cs
@@ -20,30 +20,23 @@ namespace YOBAGame.MapObjects
         }
 
         private Angle Scatter { get; }
-        public override Tuple<Bitmap, Point>[][] Images { get; }
-        public override string ImageFileName { get; }
-        public UsualBullet Ammo { get; private set; }
         public override Resources Resources { get; }
 
         public UsualWeapon(IShape hitBox, IGameRules rules, double reloadDuration, UsualBullet bullet,
-            int bulletsNumber,
+            int bulletsNumber, Resources resources,
             Angle scatter) : base(hitBox, rules)
-        public UsualWeapon(IShape hitBox, IGameRules rules, UsualBullet bullet, 
-            Resources resources) : base(hitBox, rules)
         {
             Ammo = bullet;
             BulletsNumber = bulletsNumber;
             Scatter = scatter;
             ReloadDuration = reloadDuration;
-            ImageFileName = "weapon1_sprites.png";
-            Images = Game.pictures[ImageFileName];
             Resources = resources;
         }
 
         public UsualWeapon(UsualWeapon weapon)
             : this(
                 weapon.HitBox, weapon.Rules, weapon.ReloadDuration, weapon.Ammo, weapon.BulletsNumber,
-                weapon.Scatter)
+                weapon.Resources, weapon.Scatter)
         {
         }
 
