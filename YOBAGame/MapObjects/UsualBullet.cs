abd7074afe48545761720c8745c22117461eccc5
Исламов Айдар Альбертович <lowgear1000@gmail.com>
2017-05-18 16:12:37 +0500

    Guns now can fire bullets

--- YOBAGame/MapObjects/UsualBullet.cs
+++ YOBAGame/MapObjects/UsualBullet.cs
@@ -14,6 +14,7 @@ namespace YOBAGame.MapObjects
         private readonly Vector2 _speed;
         public string ImageFileName { get; }
         public Tuple<Bitmap, Point>[][] Images { get; }
+
         IEnumerable<Tuple<Bitmap, Point>> IDrawableObject.ForDrawing
         {
             get
@@ -21,23 +22,29 @@ namespace YOBAGame.MapObjects
                 var relative = Owner.IsRightSide() ? Angle.Zero : Angle.HalfRotation;
                 var pic = Images[0][0].Item1.RotateImage((Owner.Direction - relative).Degrees);
                 var loc = new Point(
-                    (int)Owner.Coordinates.X + Images[0][0].Item2.X,
-                    (int)Owner.Coordinates.X + Images[0][0].Item2.X);
+                    (int) Owner.Coordinates.X + Images[0][0].Item2.X,
+                    (int) Owner.Coordinates.X + Images[0][0].Item2.X);
                 return new[] {Tuple.Create(pic, loc)};
             }
         }
 
         private readonly int _scaleCoefficient; // TODO: настроить
 
-        public UsualBullet(Vector2 coordinates, Vector2 speed, IShape hitBox, AbstractUnit owner, IGameRules rules, int damage = int.MaxValue)
+        public UsualBullet(Vector2 coordinates, Vector2 speed, IShape hitBox, AbstractUnit owner,
+            IGameRules rules, int damage = int.MaxValue)
             : base(hitBox, owner, rules, damage)
         {
             Coordinates = coordinates;
             _speed = speed;
-            
+
             _scaleCoefficient = 5;
             ImageFileName = "bullet1_sprites.png";
             Images = Game.pictures[ImageFileName];
+        }
+
+        public UsualBullet(UsualBullet bullet, Vector2 coordinates, Vector2 speed)
+            : this(coordinates, speed, bullet.HitBox, bullet.Owner, bullet.Rules, bullet.Damage)
+        {
         }
 
         public override bool ShouldBeDeleted { get; set; }
@@ -49,6 +56,7 @@ namespace YOBAGame.MapObjects
         }
 
         public override Vector2 Coordinates { get; set; }
+
         public override void Decide(double dt, GameState gameState)
         {
         }
--- YOBAGame/MapObjects/UsualWeapon.cs
+++ YOBAGame/MapObjects/UsualWeapon.cs
@@ -6,28 +6,43 @@ using System.Text;
 using System.Threading.Tasks;
 using YOBAGame.GameRules;
 using Archimedes.Geometry;
+using Archimedes.Geometry.Units;
 
 namespace YOBAGame.MapObjects
 {
     public class UsualWeapon : AbstractWeapon
     {
+        private int _bulletsNumber;
         public UsualBullet Ammo { get; private set; }
+
+        public int BulletsNumber
+        {
+            get { return _bulletsNumber; }
+            set { _bulletsNumber = value > 0 ? value : 1; }
+        }
+
+        public Angle Scatter { get; private set; }
         public override Tuple<Bitmap, Point>[][] Images { get; }
         public override string ImageFileName { get; }
 
-        public UsualWeapon(IShape hitBox, IGameRules rules, UsualBullet bullet) : base(hitBox, rules)
+        public UsualWeapon(IShape hitBox, IGameRules rules, UsualBullet bullet, int bulletsNumber,
+            Angle scatter) : base(hitBox, rules)
         {
             Ammo = bullet;
+            BulletsNumber = bulletsNumber;
+            Scatter = scatter;
             ImageFileName = "weapon1_sprites.png";
             Images = Game.pictures[ImageFileName];
         }
 
-        public UsualWeapon(UsualWeapon weapon) : this(weapon.HitBox, weapon.Rules, weapon.Ammo)
+        public UsualWeapon(UsualWeapon weapon)
+            : this(weapon.HitBox, weapon.Rules, weapon.Ammo, weapon.BulletsNumber, weapon.Scatter)
         {
         }
 
         public override Vector2 Coordinates { get; set; }
         public override Vector2 Speed { get; set; }
+
         public override IEnumerable<IMapObject> GeneratedObjects()
         {
             return Enumerable.Empty<IMapObject>();
@@ -38,7 +53,24 @@ namespace YOBAGame.MapObjects
             return Enumerable.Empty<IMapObject>();
         }
 
-        protected override IEnumerable<IBullet> FiredBullets { get; }
+        protected override IEnumerable<IBullet> FiredBullets
+        {
+            get
+            {
+                var speed = (Owner is Player) ? Rules.PlayerBulletSpeed : Rules.BotBulletSpeed;
+                if (BulletsNumber == 1)
+                {
+                    yield return new UsualBullet(Ammo, Owner.Coordinates, Vector2.FromAngleAndLenght(Owner.Direction, speed));
+                    yield break;
+                }
+                var anglePiece = Scatter / (BulletsNumber - 1);
+                var startAngle = Owner.Direction - Scatter / 2;
+                foreach (var i in Enumerable.Range(0, BulletsNumber))
+                    yield return new UsualBullet(Ammo, Owner.Coordinates, Vector2.FromAngleAndLenght(startAngle + i * anglePiece, speed));
+            }
+
+        }
+
         protected override double ReloadDuration { get; }
     }
 }
